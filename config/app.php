0d3287890eeec36a8660692aecf17cdb78681ba4
Fernando De Los Santos Malpica <fernando_santosmalpica@outlook.com>
2026-03-24 17:33:17 -0600

    (config) dotenv and app bootstrapping

--- config/app.php
+++ config/app.php
@@ -1,4 +1,13 @@
 <?php
 
+require __DIR__ . "/../vendor/autoload.php";
+
+use Dotenv\Dotenv;
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
+
 require __DIR__ . "/../helpers/functions.php";
 require __DIR__ . "/database.php";
+
+use Core\ActiveRecord;
+ActiveRecord::setDB($db);
