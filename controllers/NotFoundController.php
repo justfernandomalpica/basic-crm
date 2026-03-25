78d3240e8a134441a12951f67375513a60948891
Fernando De Los Santos Malpica <fernando_santosmalpica@outlook.com>
2026-03-25 00:38:08 -0600

    (fix) NotFoundController stops runtime execution whenever router did't resolve current uri

--- controllers/NotFoundController.php
+++ controllers/NotFoundController.php
@@ -7,6 +7,6 @@ class NotFoundController {
         echo "<h2>Error de direccionamiento</h2>";
         echo "<h1>404</h1>";
         echo "<h3>Página no encontrada</h3>";
-        return;
+        exit;
     }
 }
