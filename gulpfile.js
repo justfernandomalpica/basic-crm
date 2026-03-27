6f49c8a4fb4670838e137e57e414484b2062008b
Fernando De Los Santos Malpica <fernando_santosmalpica@outlook.com>
2026-03-27 00:38:28 -0600

    (feat) JS bundling added using ESBuild

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
-import { dest, src, series, watch } from "gulp";
-import dartSass from "sass";
+import { dest, src, series, parallel, watch } from "gulp";
+import * as dartSass from "sass";
 import gulpSass from "gulp-sass";
+import esbuild from "gulp-esbuild";
 
 const sass = gulpSass(dartSass);
 
@@ -10,22 +11,41 @@ const paths = {
     watch: "./src/sass/**/*.scss",
     dest: "./public/build/css",
   },
+  scripts: {
+    entry: "./src/js/app.js",
+    watch: "./src/js/**/*.js",
+    dest: "./public/build/js",
+    bundleName: "bundle.min.js",
+  },
 };
 
 function buidlStyles() {
   return src(paths.styles.entry)
     .pipe(
       sass({
-        style: "compressed",
+        style: "compressed", // "compressed" or "expanded"
       }).on("error", sass.logError),
     )
     .pipe(dest(paths.styles.dest));
 }
 
-function watcher() {
-  watch(paths.styles.watch, buidlStyles);
+async function buildScripts() {
+  return src(paths.scripts.entry)
+    .pipe(
+      esbuild({
+        outfile: paths.scripts.bundleName,
+        bundle: true,
+        minify: true,
+        platform: "browser",
+      }),
+    )
+    .pipe(dest(paths.scripts.dest));
 }
 
-export { buidlStyles };
-export const dev = series(buidlStyles, watcher);
-export const build = buidlStyles;
+function watcher() {
+  watch(paths.styles.watch, buidlStyles);
+  watch(paths.scripts.watch, buildScripts);
+}
+
+export const build = parallel(buidlStyles, buildScripts);
+export const dev = series(build, watcher);
