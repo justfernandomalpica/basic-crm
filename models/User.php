88399908c4f0439faa2440a43695f5eaae736cde
Fernando De Los Santos Malpica <fernando_santosmalpica@outlook.com>
2026-03-27 12:52:21 -0600

    (fix) Added role validation and strong typing to model properties

--- models/User.php
+++ models/User.php
@@ -8,12 +8,12 @@ class User extends ActiveRecord {
     protected static $table = 'users';
     protected static $columns = ['id', 'name','email','password','role','token','isConfirmed'];
 
-    public $name;
-    public $email;
-    public $password;
-    public $role;
-    public $token;
-    public $isConfirmed;
+    public string $name;
+    public string $email;
+    public string $password;
+    public string $role;
+    public ?string $token;
+    public int $isConfirmed;
 
     public function __construct(array $args) {
         $this->validate($args);
@@ -30,7 +30,8 @@ class User extends ActiveRecord {
         if(empty($args)) throw new \InvalidArgumentException($baseErrorMsg."Class cannot be instanced with empty arguments array");
         foreach($args as $key => $value){
             if(!in_array($key, self::$columns)) throw new \InvalidArgumentException($baseErrorMsg."'{$key}' is not a column in SQL table");
-            if($value === '') throw new \InvalidArgumentException($baseErrorMsg."'{$key} cannot be empty");
+            if($value === '') throw new \InvalidArgumentException($baseErrorMsg."'{$key}' cannot be empty");
+            if($key === 'role' && !in_array($value,['admin','user'])) throw new \InvalidArgumentException($baseErrorMsg."Invalid user role");
         }
     }
 }
